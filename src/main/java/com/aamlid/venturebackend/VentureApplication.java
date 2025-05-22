d794d3c0af2a80e16ffd8579aa3f0faf08299b1f
thesupermattman <matthew.aamlid@hotmai.com>
2025-05-23 01:32:54 +0700

    Rename IntelliJ module to venture-backend

--- src/main/java/com/aamlid/venturebackend/VentureApplication.java
+++ src/main/java/com/aamlid/venturebackend/VentureApplication.java
@@ -1,4 +1,4 @@
-package com.aamlid.venture;
+package com.aamlid.venturebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/aamlid/venturebackend/VentureApplicationTests.java
+++ src/test/java/com/aamlid/venturebackend/VentureApplicationTests.java
@@ -1,4 +1,4 @@
-package com.aamlid.venture;
+package com.aamlid.venturebackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
